8714b937d25affdf8b83e71ee6d58ee3263ac7c0
Alper Karaca <alpeerkaraca@gmail.com>
2023-07-07 15:57:22 +0300

    Refactored

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,52 @@
+# frozen_string_literal: true
+
+# Inherits from ApplicationController
 class ArticlesController < ApplicationController
-  def show
+  before_action :find_article, only: %i[show edit update destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Article was created successfully.' # alert available too
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def destroy
+    @article.delete
+    redirect_to articles_path # redirect to the article list
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = 'Article was updated successfully.'
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def find_article
     @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
-  resources :articles, only: [:show]
-
+  resources :articles,  only: [:show, :index, :create, :new, :edit, :update] #cause of we are using all of capabilities of CRUD will use just resoruces :articles thats how we get all
+  delete 'articles/:id' => 'articles#destroy', as: :delete_article #this is how we get delete route
 end
